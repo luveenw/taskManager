883d1c733751576ee1a1f6c1aa4b6544680622b8
Luveen Wadhwani <luveenw@gmail.com>
2012-03-31 17:38:06 -0500

    commit - init

---
+++ app/modules/example.js
@@ -0,0 +1,47 @@
+define([
+  ".",
+
+  // Libs
+  "use!backbone"
+
+  // Modules
+
+  // Plugins
+],
+
+function(., Backbone) {
+
+  // Shorthand the application namespace
+  var app = ..app;
+
+  // Create a new module
+  var Example = ..module();
+
+  // Example extendings
+  Example.Model = Backbone.Model.extend({ /* ... */ });
+  Example.Collection = Backbone.Collection.extend({ /* ... */ });
+  Example.Router = Backbone.Router.extend({ /* ... */ });
+
+  // This will fetch the tutorial template and render it.
+  Example.Views.Tutorial = Backbone.View.extend({
+    template: "app/templates/example.html",
+
+    render: function(done) {
+      var view = this;
+
+      // Fetch the template, render it to the View element and call done.
+      ..fetchTemplate(this.template, function(tmpl) {
+        view.el.innerHTML = tmpl();
+
+        // If a done function is passed, call it with the element
+        if (_.isFunction(done)) {
+          done(view.el);
+        }
+      });
+    }
+  });
+
+  // Required, return the module for AMD compliance
+  return Example;
+
+});
